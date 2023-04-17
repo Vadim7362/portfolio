9f7b707256254b894861b7602c7d4a3e6f4b6cf3
Vadim Cherepanov <Vadim5005473@mail.ru>
2023-04-17 16:24:45 +0300

    add burger-menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 document.getElementById("info-button"). onclick = function () {
   document.getElementById("contacts").scrollIntoView({behavior: "smooth"});
 };
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("burger").addEventListener("click", function() {
+    document.querySelector("header").classList.toggle("open")
+  })
+})
